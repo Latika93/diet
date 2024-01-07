ac69cf53c63ce1c353876206b3e4e27cbcb0bcab
Latika Verma <vermalatika8@gmail.com>
2024-01-07 19:18:46 +0530

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+import AboutBackground from "../Assets/bg2.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        {/* <img src={AboutBackground} alt="" /> */}
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Nourishing Your Lifestyle
+        </h1>
+        <p className="primary-text">
+        Explore our carefully curated nutrition plans designed to nourish your unique lifestyle.
+        </p>
+        <p className="primary-text">
+        From balanced meal suggestions to personalized dietary advice, find the key to unlocking your best self.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BannerBackground from "../Assets/bg.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Welcome to WebWeavePro. Hub
+          </h1>
+          <p className="primary-text">
+            Step into a world of holistic health with NutriWellness Hub, your ultimate destination for personalized nutrition guidance. Let us accompany you on your journey to a healthier, happier you.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
